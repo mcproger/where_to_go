acc6d63ee4b1ff24f567f8227fbf6d384a8d1cce
mcproger <mcproger7@gmail.com>
2022-06-24 13:49:43 +0300

    Add image preview to the place admin

--- src/places/admin.py
+++ src/places/admin.py
@@ -1,10 +1,27 @@
+from django.http.request import HttpRequest
+from django.utils.safestring import mark_safe
+
 from app.admin import ModelAdmin, admin
+from app.models import models
 from places.models import Place, PlaceImage
 
 
 class PlaceImageInline(admin.TabularInline):
     model = PlaceImage
     extra = 3
+    fields = [
+        'title',
+        'render_image',
+        'order',
+    ]
+    readonly_fields = [
+        'render_image',
+    ]
+
+    def render_image(self, obj):
+        return mark_safe(
+            f'<img src="{obj.image.url}" height="200px" />'
+        )
 
 
 @admin.register(PlaceImage)
@@ -17,3 +34,7 @@ class PlaceAdmin(ModelAdmin):
     inlines = [
         PlaceImageInline,
     ]
+
+    def get_queryset(self, request: HttpRequest) -> models.QuerySet:
+        queryset = super().get_queryset(request)
+        return queryset.prefetch_related('images')
--- src/places/api/views.py
+++ src/places/api/views.py
@@ -1,8 +1,8 @@
 from typing import Any
 
 from django.shortcuts import get_object_or_404
-from rest_framework.response import Response
 from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from places.api.serializers import PlaceSerializer
